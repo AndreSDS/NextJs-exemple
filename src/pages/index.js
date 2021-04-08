c5b11494cec3b0f690c9f2abbd693aad8b8f1f6d
andre <andre_smiths@outlook.com>
2021-04-08 17:29:35 -0300

    Finalizando teste com items mockados

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,6 +50,16 @@ export async function getStaticProps() {
       views: 54000, 
       url: 'url',
       updatedAt: new Date(),
+     },
+     {
+      id: 4,
+      title: 'Olá teste',
+      authorName: "André",
+      thumb: 'https://img.freepik.com/free-photo/emale-hand-showing-thumbs-up-gesture_77190-292.jpg?size=626&ext=jpg&ga=GA1.2.1356500412.1617148800',
+      avatar: 'https://www.shareicon.net/data/512x512/2016/05/24/770117_people_512x512.png',
+      views: 54000, 
+      url: 'url',
+      updatedAt: new Date(),
      }
   ];
 
