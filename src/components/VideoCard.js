6a0882e5dd2c1e9ed02edc9372406e9a5144b724
andre <andre_smiths@outlook.com>
2021-04-08 17:21:08 -0300

    criando componente VideoCard para exibição dos cards.

---
+++ src/components/VideoCard.js
@@ -0,0 +1,53 @@
+import { Box, Typography, Avatar, makeStyles } from '@material-ui/core';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+const useStyles = makeStyles((theme) => ({
+  caption: {
+    fontWeight: 500,
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden'
+    },
+    img: {
+      width: '100%'
+      },
+}));
+
+function VideoCard({ item }) {
+  const { caption, img } = useStyles();
+
+  return (
+    <Box>
+      <img className={img} alt={item.title} src={item.thumb} />
+      <Box display="flex" mt="1">
+        <Box>
+          <Avatar alt={item.authorName} src={item.avatar}>
+            SS
+          </Avatar>
+        </Box>
+        <Box>
+          <Typography
+            className={caption}
+            gutterBottom
+            variant="body2"
+            color="textPrimary"
+          >
+            {item.title}
+          </Typography>
+          <Typography display="bolck" variant="body2" color="textSecondary">
+            {item.authorName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">{`${
+            item.views
+          } - ${dayjs(item.updatedAt).fromNow()}`}</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default VideoCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Box, Grid } from '@material-ui/core';
 import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="Youtube">
-      <Button variant="outlined" color="secondary">
-        Hello
-      </Button>
-      Clone Youtube com NextJs
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+     id: 1,
+     title: 'Olá teste',
+     authorName: "André",
+     thumb: 'https://img.freepik.com/free-photo/emale-hand-showing-thumbs-up-gesture_77190-292.jpg?size=626&ext=jpg&ga=GA1.2.1356500412.1617148800',
+     avatar: 'https://www.shareicon.net/data/512x512/2016/05/24/770117_people_512x512.png',
+     views: 54000, 
+     url: 'url',
+     updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Olá teste',
+      authorName: "André",
+      thumb: 'https://img.freepik.com/free-photo/emale-hand-showing-thumbs-up-gesture_77190-292.jpg?size=626&ext=jpg&ga=GA1.2.1356500412.1617148800',
+      avatar: 'https://www.shareicon.net/data/512x512/2016/05/24/770117_people_512x512.png',
+      views: 54000, 
+      url: 'url',
+      updatedAt: new Date(),
+     },
+     {
+      id: 3,
+      title: 'Olá teste',
+      authorName: "André",
+      thumb: 'https://img.freepik.com/free-photo/emale-hand-showing-thumbs-up-gesture_77190-292.jpg?size=626&ext=jpg&ga=GA1.2.1356500412.1617148800',
+      avatar: 'https://www.shareicon.net/data/512x512/2016/05/24/770117_people_512x512.png',
+      views: 54000, 
+      url: 'url',
+      updatedAt: new Date(),
+     }
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
